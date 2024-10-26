cfe7bb09156f112e70c53d78986e4a2ec85fde2f
OnurAydin02 <07onuraydin@gmail.com>
2024-10-26 22:07:52 +0300

    Razor Pizzaria
    It is a pizza ordering project, and it was also the first project I developed while learning ASP.NET MVC.

---
+++ Razor Pizzaria/FirstMVC-App/FirstMVC-App/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVC_App.Models;
+
+namespace FirstMVC_App.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            // return View ("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello() 
+        {
+            return "Who's there?";
+        }
+
+    }
+}
